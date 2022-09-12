ed8a7a81965099777a7ae700767b496631525bc8
lutinwa <lennoxlutinwa@gmail.com>
2022-09-12 10:51:51 -0700

    new file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int number;
+
+	for (number = 0 ; number < 10 ; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+
+	return (0);
+}
