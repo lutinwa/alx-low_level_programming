7bc8380548d3e34611309e0206b4f10baaceb1a3
lutinwa <lennoxlutinwa@gmail.com>
2022-09-13 10:12:19 -0700

    new file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Description: 'print alphabet in lower case'
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
+
