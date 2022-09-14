d0d6e9f8a77ec96b30809a077a6713ce173cf7e5
lutinwa <lennoxlutinwa@gmail.com>
2022-09-14 07:51:00 -0700

    new file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,7 +18,7 @@ int main(void)
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	A
+	
 	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 + fib2;
