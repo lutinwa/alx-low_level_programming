45de55db0918cc0a5703813b7620fd7f9b2783f6
lutinwa <lennoxlutinwa@gmail.com>
2022-09-15 13:05:44 -0700

    new file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int number = 0;
+
+	positive_or_negative(number);
+	return (0);
+}
